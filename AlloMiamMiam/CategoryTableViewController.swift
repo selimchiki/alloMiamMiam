858ec44650da4494116fcedb8e6b5b5617005c44
Selim Chiki <selimchiki@yahoo.fr>
2017-09-27 16:43:50 +0200

    Modification du model pour avoir des avantages pour plus tard

--- AlloMiamMiam/CategoryTableViewController.swift
+++ AlloMiamMiam/CategoryTableViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class CategoryTableViewController: UITableViewController {
 
-    let menuController = MenuController()
+    
     var categories = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        menuController.fetchCategories { (categories) in
+        MenuController.shared.fetchCategories { (categories) in
             if let categories = categories {
                 self.updateUI(with: categories)
             }
--- AlloMiamMiam/MenuTableViewController.swift
+++ AlloMiamMiam/MenuTableViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class MenuTableViewController: UITableViewController {
     
-    let menuController = MenuController()
     var menuItems = [MenuItem]()
     var category: String!
 
@@ -18,7 +17,7 @@ class MenuTableViewController: UITableViewController {
         super.viewDidLoad()
         
         title = category.capitalized
-        menuController.fetchMenuItems(categoryName: category)
+        MenuController.shared.fetchMenuItems(categoryName: category)
         { (menuItems) in
             if let menuItems = menuItems {
                 self.updateUI(with: menuItems)
