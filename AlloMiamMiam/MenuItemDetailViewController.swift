fbbadbe4687323ea1f827ef401ee1841b44e544e
Selim Chiki <selimchiki@yahoo.fr>
2017-09-27 17:06:20 +0200

    F - Création de la vue de detail du plat
    Création de la vue pour afficher le details d'un plat.

--- AlloMiamMiam/MenuItemDetailViewController.swift
+++ AlloMiamMiam/MenuItemDetailViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class MenuItemDetailViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
+    
     var menuItem: MenuItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         // Do any additional setup after loading the view.
+    }
+    
+    func updateUI() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        descriptionLabel.text = menuItem.description
     }
 
     override func didReceiveMemoryWarning() {
